a92146e8662674dd9ea3b7c1fe94655b0e9af1d9
Nxtrik <33619208+Nxtrik@users.noreply.github.com>
2019-01-16 10:46:25 +0100

    home.php
    View home created

---
+++ laravel/resources/views/home.php
@@ -0,0 +1,20 @@
+@extends ("template")
+
+@section('content')
+
+{!! Form::open(['url'=>'/Connexion'])!!}
+
+	<h3> Qu'est ce qu'un BDE </h3>
+
+	<div>
+
+		Voici un texte de test histoire de voir si tout va bien.
+		tout va bien.
+		Voici un texte de test histoire de voir si tout va bien.
+		tout va bien.
+
+	</div>
+
+{!! Form::close()!!}
+
+@endsection
