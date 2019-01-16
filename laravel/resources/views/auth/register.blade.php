2bd39329a0968923ed0f6485687d0b3ede0eeaf7
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-01-16 13:54:39 +0100

    Register and login system work

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -12,30 +11,51 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('first_name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                                <input id="first_name" type="text" class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name"
+                                    value="{{ old('first_name') }}" required autofocus> @if ($errors->has('first_name'))
+                                <span class="invalid-feedback" role="alert">
+										<strong>{{ $errors->first('first_name') }}</strong>
+									</span> @endif
                             </div>
                         </div>
+
+                        <div class="form-group row">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('last_name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name"
+                                    value="{{ old('last_name') }}" required autofocus> @if ($errors->has('last_name'))
+                                <span class="invalid-feedback" role="alert">
+								<strong>{{ $errors->first('last_name') }}</strong>
+								</span> @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="center" class="col-md-4 col-form-label text-md-right">{{ __('center') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="center" type="text" class="form-control{{ $errors->has('center') ? ' is-invalid' : '' }}" name="center" value="{{ old('center') }}"
+                                    required autofocus> @if ($errors->has('center'))
+                                <span class="invalid-feedback" role="alert">
+                        								<strong>{{ $errors->first('center') }}</strong>
+                        								</span> @endif
+                            </div>
+                        </div>
+
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
+                                    required> @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+										<strong>{{ $errors->first('email') }}</strong>
+									</span> @endif
                             </div>
                         </div>
 
@@ -43,13 +63,11 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                                    required> @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+										<strong>{{ $errors->first('password') }}</strong>
+									</span> @endif
                             </div>
                         </div>
 
@@ -64,8 +82,8 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+									{{ __('Register') }}
+								</button>
                             </div>
                         </div>
                     </form>
