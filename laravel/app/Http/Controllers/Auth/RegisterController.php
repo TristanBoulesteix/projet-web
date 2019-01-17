4479dd182fc9c09aa839250b23cc3a4f616e3657
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-01-17 17:22:53 +0100

    Connection moved to another database

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller {
 			'first_name' => ['required', 'string', 'max:255'],
 			'last_name' => ['required', 'string', 'max:255'],
 			'campus' => ['string', 'max:255'],
-			'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+			'email' => ['required', 'string', 'email', 'max:255', 'unique:mysql_user.users'],
 			'password' => ['required', 'string', 'min:6', 'confirmed', 'max:255', 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/'],
 		]);
 	}
