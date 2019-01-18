db7735d45b9e19e8f51db73cd7d5f45e86d0aa50
DrakLulu <charlotte.madroux@gmail.com>
2019-01-18 17:02:41 +0100

    Node.js API
    Add the begening of the Node.js API

--- Node/server.js
+++ Node/server.js
@@ -1,20 +1,118 @@
-var http = require('http');
-var url = require('url');
+//the API require the Express Module
+var express = require('express');
 
-var server = http.createServer((req, res) =>{
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/plain"});
+//Variables to set the Express Server
+var express = require('express');
+var hostname = 'localhost';
+var port = 3000;
 
-    if (page == '/') {
-        res.write('Voici la premiere page du serveur Node.js.');
-    }
-    else if (page == '/page2') {
-        res.write('Vous etes sur la deuxieme page du serveur Node.js .');
-    }
-    else if (page == '/page3') {
-        res.write('Vous etes sur la 3eme page.');
-    } else res.write('Voici la premiere page du serveur Node.js. Il en existe 2 autres.');
-    res.end();
+//the express application
+var app = express();
+// Express router
+var myRouter = express.Router();
+
+//Init of the body-Parser
+var bodyParser = require("body-parser"); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//MySql connection
+var mysql = require('mysql');
+//connexion to the database
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "projet-web"
+  });
+  
+
+myRouter.route('/')
+.all(function(req,res){
+      res.json({message : "Vous êtes sur le server Web Services Express du site internet du BDE ", methode : req.method});
 });
-server.listen(8080);
+
+myRouter.route('/events')
+// GET
+.get(function(req,res){
+    var participate = 22;
+    var reslt;
+    var sql = "SELECT name FROM `campus` WHERE id = 1";
+
+  /*  var promise = new Promise(function(res, rej){
+        con.connect(function(err) {
+            if (err) throw err;
+            con.query(sql, function (err, result, fields) {
+              if (err) throw err;
+              reslt = result[0].name;
+              console.log(reslt);
+            });
+          });
+        res(reslt);
+    });*/
+    var promise1 = new Promise(function(resolve, reject) {
+        setTimeout(function() { 
+        con.connect(function(err) {
+            if (err) throw err;
+            con.query(sql, function (err, result, fields) {
+              if (err) throw err;
+              reslt = result[0].name;
+              console.log(reslt);
+            });
+          });
+          resolve(reslt);
+        }, 1000);
+
+      });
+      
+      promise1.then(function(value) {
+        res.json({
+        message : "route get :",
+        name: req.query.name,
+        date: req.query.date,
+        campus : value,
+        participants: participate,
+        methode: req.method});
+      });
+    
+    /*promise.then(function(){
+      res.json({
+      message : "route get :",
+      name: req.query.name,
+      date: req.query.date,
+      campus : reslt,
+      participants: participate,
+      methode: req.method});
+    });*/
+})
+//POST
+.post(function(req,res){
+      res.json({message : "route post",
+      name: req.query.name,
+      date: req.query.date,
+      participants: '12',
+      methode : req.method});
+})
+//PUT
+.put(function(req,res){
+      res.json({message : "route put",
+      name: req.query.name,
+      date: req.query.date,
+      methode : req.method});
+})
+//DELETE
+.delete(function(req,res){
+res.json({message : "route delete",
+      name: req.query.name,
+      date: req.query.date,
+      methode : req.method});
+});
+
+// Nous demandons à l'application d'utiliser notre routeur
+app.use(myRouter);
+
+
+// Starts the server
+app.listen(port, hostname, function(){
+	console.log("Server on http://"+ hostname +":"+port+"\n");
+});
