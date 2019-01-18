d8fdaff563db23e3b279fafd4f7e63874d8602ec
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-01-18 09:55:42 +0100

    Merge branch 'Feat.Tristan' into Dev
    - Add new migrations

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration {
 			$table->string('status');
 			$table->integer('campus');
 			$table->foreign('campus')->references('id')->on('campus');
-			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->rememberToken();
 			$table->timestamps();
