b5b07700729392ade3ba515f52eb6af58c37f45a
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-01-18 16:04:47 +0100

    New controller created

---
+++ laravel/app/Http/Controllers/ShopController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ShopController extends Controller {
+	public function __construct() {
+		$this->middelware('auth');
+	}
+
+	public function
+}
