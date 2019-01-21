f763a084aaad13b06b7816b8936ebdb339c57b44
Nxtrik <33619208+Nxtrik@users.noreply.github.com>
2019-01-21 15:44:15 +0100

    maj css

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,18 +1,5 @@
 @extends ("template")
 
-@section('css')
-<style>
-	main {
-		height: 70.4vh;
-	}
-
-	@-moz-document url-prefix() {
-		main {
-			height: 71.2vh;
-		}
-	}
-</style>
-@endsection
 
 @section('content')
 
@@ -20,12 +7,19 @@
 
 	<p>
 
-		Voici un texte de test histoire de voir si tout va bien.
-		tout va bien.
-		Voici un texte de test histoire de voir si tout va bien.
-		tout va bien.
+		Le Bureau des Étudiants (BDE pour les intimes) est piloté par une fine équipe d’étudiants qui feront en sorte de rendre les quelques années que vous passerez ici, conviviales et animées ! Vous pourrez nous rencontrer au QG du BDE ou vous trouverez a certaines heures des membres du BDE.
+		Ce sera l’occasion de faire des rencontres, de boire le meilleur café du monde, de trouver de l’aide face à tous les soucis de la vie étudiante ou encore de discuter du prochain évènement du BDE !
 
 	</p>
 
+	<h4> Pourquoi rejoindre le BDE? </h4>
+
+	<p>
+		
+		Vous engager dans le BDE est l’occasion idéale pour rencontrer des gens au début de ses études, en effet, la plupart du temps les personnes que vous allez rencontrer sont sociables et n’ont pas peur d’aller vers les autres pour faire leur connaissance. Cela va également être l’occasion pour faire la fête, sortir de la routine quotidienne et de décompresser face aux cours !
+
+	</p>
+
+	<img id="bde" src="../img/lyon.png">
 
 @endsection
