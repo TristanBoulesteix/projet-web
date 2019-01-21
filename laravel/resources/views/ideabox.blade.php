e75c84002926adf7b1e320ab2ef7a6c202e501f0
Nxtrik <33619208+Nxtrik@users.noreply.github.com>
2019-01-21 13:11:44 +0100

    ideabox

--- laravel/resources/views/ideabox.blade.php
+++ laravel/resources/views/ideabox.blade.php
@@ -2,11 +2,7 @@
 @section ('css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="./css/ideabox.css">
-<style>
-	main {
-		height: 65%;
-	}
-</style>
+
 @endsection
 
 @section ('content')
@@ -23,8 +19,29 @@
 	  	<p> description de l'idée </p>
 	    </div>
 	    <div id="buttonCase">
-	    <div></div>
-	    <div></div>
+	    <a id="but" href=""> si bde: administrer </a>
+	    </div>
+	</div>
+	<div id="likeButton"><i class="fa fa-thumbs-up"></i></div>
+	<div class="row">
+    	<div id="imgcase">
+   		</div>
+        <div class="content">
+	  	<p> description de l'idée </p>
+	    </div>
+	    <div id="buttonCase">
+	    <a id="but" href=""> si bde: administrer </a>
+	    </div>
+	</div>
+	<div id="likeButton"><i class="fa fa-thumbs-up"></i></div>
+	<div class="row">
+    	<div id="imgcase">
+   		</div>
+        <div class="content">
+	  	<p> description de l'idée </p>
+	    </div>
+	    <div id="buttonCase">
+	    <a id="but" href=""> si bde: administrer </a>
 	    </div>
 	</div>
 	<div id="likeButton"><i class="fa fa-thumbs-up"></i></div>
