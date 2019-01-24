44b7b085be370e543f4e6e2eeeffcf2f94624adf
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-01-24 19:46:25 +0100

    Password without lowercase letter are now allowed

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller {
 			'last_name' => ['required', 'string', 'max:255'],
 			'campus' => ['string', 'max:255'],
 			'email' => ['required', 'string', 'email', 'max:255', 'unique:mysql_user.users'],
-			'password' => ['required', 'string', 'min:6', 'confirmed', 'max:255', 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/'],
+			'password' => ['required', 'string', 'min:6', 'confirmed', 'max:255', 'regex:/^(?=.*[A-Z])(?=.*\d).+$/'],
 		]);
 	}
 
