e26220cc9be60f018de7b02acb3f1e126eaec5bf
Nxtrik <33619208+Nxtrik@users.noreply.github.com>
2019-01-28 11:50:56 +0100

    test ideabox

--- sql/roleperID.sql
+++ sql/roleperID.sql
@@ -2,7 +2,7 @@ DELIMITER |
 CREATE PROCEDURE rolPerUser(role_id INT)
 
 BEGIN
-	SELECT `role_name`
-	FROM `role`
-	WHERE `id_role`= role_id;
+	SELECT `role`
+	FROM `roles`
+	WHERE `id`= role_id;
 END|
