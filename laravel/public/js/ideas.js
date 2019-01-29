c0112f19500e9f5384450544f92e0c4c93780fb5
DrakLulu <charlotte.madroux@gmail.com>
2019-01-29 15:24:56 +0100

    add admin button

--- laravel/public/js/ideas.js
+++ laravel/public/js/ideas.js
@@ -67,23 +67,7 @@ function displayOn(myJSON) {
 	var row = 0;
 //for each part of teh json, create HTML elements
 	for (var i = 0; i < json.length; i++) {
-	  currentRow = $(document.createElement("div")).addClass("row").attr("id", "row"+row);
-		wrapper.append(currentRow);
-		var img = $(document.createElement("div")).attr("style", "background-image: url('storage/idea/"+json[i].image+"')").attr("alt", "image idée").attr("class", "imgcase");
-    currentRow.append(img);
-    var reportbtn = $(document.createElement("a")).addClass('buttonReport').text("report").attr("href", "/home");
-    img.append(reportbtn);
-		var content = $(document.createElement("div")).addClass("content");
-		currentRow.append(content);
-		content.append('<h3>'+ json[i].name + '</h3>');
-		content.append("<p>"+json[i].description +"</p>");
-		var hidebuttons = $(document.createElement("div")).attr("id", "buttonCase");
-		content.append(hidebuttons);
-		var like = $(document.createElement("div")).addClass("likeButton");
-		wrapper.append(like);
-		var bouton = $(document.createElement("i")).addClass("fa fa-thumbs-up").attr("id", json[i].id).attr("onclick", "clicked("+json[i].id+")");
-		like.append(bouton);
-		column = 1;
+      displayideas(wrapper, json, i, row, currentRow)
 		row ++;
   }
   // add an events listeners hover to display or not the buttons
