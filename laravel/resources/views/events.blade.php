6e10105e577758300e150c7aebbd24ecc6397f76
DrakLulu <charlotte.madroux@gmail.com>
2019-01-29 13:24:49 +0100

    add download option

--- laravel/resources/views/events.blade.php
+++ laravel/resources/views/events.blade.php
@@ -53,6 +53,9 @@ function createElement(currentRow, json, wrapper, row, i) {
 	if(type != "none"){
 		content.append("<p> Évenement "+ type +"</p>");
 	}
+	@if($role == 'BDE')
+	content.append("<a href='"+{{url('event/download/?type=event&id='.$idEvent+}}+'\'class="button">Télécharger les inscrits</a>');
+	@endif
 	row ++;
 }
 
